8418492b5c0ef45351cd6222e5f0e12370d01627
Stanley Umeanozie <coolstanlee@gmail.com>
2018-12-24 20:09:27 +0100

    Sass styles, and other minor changes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import allReducers from './reducers/index';
+import {newPost} from './actions/index';
+// require('babel-polyfill');
+require('bootstrap/dist/css/bootstrap.min.css');
+// import $ from 'jquery';
+require('jquery');
+require('popper.js');
+require('bootstrap');
+require('./css/style.scss');
 
-ReactDOM.render('
-  <div>Hello world</div>
-', document.getElementByID('app'));
+const store = createStore(allReducers);
+
+// console.log('dispatching', 'NEW_POST');
+// store.dispatch(newPost('Okay'));
+// console.log('next state', store.getState());
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+      </div>
+    </Router>
+  </Provider>
+  , document.getElementById("app")
+);
